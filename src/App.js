8b30313bc8896b213e65b4220ca3e36175d07761
Franco Arriola <f50032702@china.huawei.com>
2024-10-14 15:56:35 -0300

    Updated favicon and index.html to display custom icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css'; // Asegúrate de personalizar los estilos en tu archivo CSS
 import placeholderImage from './assets/construction-set-icons_24877-60028.avif'; // Reemplaza con la ruta correcta de la imagen
+import QRCode from 'react-qr-code'; // Importa el componente QRCode
 
 function App() {
+  const links = [
+    'https://americantint.net/ficha-tecnica/100',
+    'https://americantint.net/ficha-tecnica/200',
+    'https://americantint.net/ficha-tecnica/300',
+    'https://americantint.net/ficha-tecnica/400',
+    'https://americantint.net/ficha-tecnica/500',
+    'https://americantint.net/ficha-tecnica/wf',
+  ];
+
   return (
     <div className="construction-page">
       <div className="left-section">
@@ -23,6 +33,21 @@ function App() {
       <footer>
         <p>© 2024 por AmericanTint. Orgullosamente creado para la empresa.</p>
       </footer>
+
+      {/* Códigos QR debajo del footer */}
+      <div className="qr-codes-section">
+        <h2>Códigos QR</h2>
+        <div className="qr-codes-container">
+          {links.map((link, index) => (
+            <div key={index} className="qr-code-card">
+              <div className="qr-code-card-content">
+                <QRCode value={link} size={150} />
+                <p>{link}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
