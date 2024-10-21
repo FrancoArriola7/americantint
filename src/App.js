8f33e90c45049a53da77b944115cf7a6f4c967ce
FrancoArriola7 <113956731+FrancoArriola7@users.noreply.github.com>
2024-10-21 15:35:47 -0300

    Update App.js with wpp link and select language

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import {FaFacebookF, FaInstagram, FaYoutube} from 'react-icons/fa';
+import {FaFacebookF, FaInstagram, FaYoutube, FaChevronDown, FaChevronUp} from 'react-icons/fa';
+import Flag from 'react-world-flags';
 
 const images = [
   require('./assets/marmoleado.png'), // Ruta de la primera imagen
   require('./assets/colorido.png'), // Ruta de la segunda imagen
   require('./assets/fondo3.png')
   ];
+
+const texts = {
+  en: {
+    comingSoon: "Coming Soon",
+    subtitle: "Our website is under construction. Stay tuned for updates!",
+    home: "Home",
+    about: "About Us",
+    contact: "Contact Us",
+  },
+  es: {
+    comingSoon: "Próximamente",
+    subtitle: "Nuestro sitio web está en construcción. ¡Mantente al tanto!",
+    home: "Inicio",
+    about: "Sobre Nosotros",
+    contact: "Contáctanos"
+  },
+  pt: {
+    comingSoon: "Em Breve",
+    subtitle: "Nosso site está em construção. Fique atento às novidades!",
+    home: "Início",
+    about: "Sobre Nós",
+    contact: "Contate-nos",
+  }
+};
+
+const flagCodes = {
+  en: 'GB', // Reino Unido como representante del inglés
+  es: 'ES', // España
+  pt: 'BR'  // Brasil
+};
 
 // Componente Typewriter para el efecto de máquina de escribir
 function Typewriter({ text, speed }) {
@@ -33,7 +64,10 @@ function Typewriter({ text, speed }) {
 
 function App() {
   const [currentImage, setCurrentImage] = useState(0);
-
+  const [currentSection, setCurrentSection] = useState(0);
+  const [language, setLanguage] = useState('en');
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // Controla el menú desplegable
+  
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImage((prevImage) => (prevImage + 1) % images.length);
@@ -41,12 +75,34 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
+  // Función para cambiar de idioma
+  const changeLanguage = (lang) => {
+    setLanguage(lang);
+    setIsDropdownOpen(false); // Cierra el menú desplegable al seleccionar un idioma
+  };
+
   return (
     <div className="background" style={{ backgroundImage: `url(${images[currentImage]})` }}>
       <header className="header">
-        <div className="logo">American Tint</div>
-        <button className="header-button">Contact Us</button>
-      </header>
+          <div className="logo">American Tint</div>
+          <div className="language-switcher" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+            <Flag code={flagCodes[language]} alt="flag" className="flag-icon" />
+            <span className="language-name">{language === 'en' ? 'English' : language === 'es' ? 'Español' : 'Português'}</span>
+            {isDropdownOpen ? <FaChevronUp className="dropdown-icon" /> : <FaChevronDown className="dropdown-icon" />}
+            {isDropdownOpen && (
+              <div className="dropdown">
+                {Object.keys(texts).map((lang) => (
+                  <div key={lang} className="dropdown-item" onClick={() => changeLanguage(lang)}>
+                    <div className="flag-container">
+                      <Flag code={flagCodes[lang]} alt={`${lang} flag`} className="dropdown-flag" />
+                      <span className="language-name">{lang === 'en' ? 'EN' : lang === 'es' ? 'ES' : 'PT'}</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </header>
       <div className="overlay">
         <div className="content">
           <h1 className="title">
@@ -65,6 +121,9 @@ function App() {
           </a>
           <a href="https://www.youtube.com/@Americantint" target="_blank" rel="noopener noreferrer">
             <FaYoutube />
+          </a>
+          <a href="https://api.whatsapp.com/send?phone=5491122496051&text=Buenas%F0%9F%91%8B%0AEstaba%20viendo%20su%20p%C3%A1gina%20web%2C%20y%20quer%C3%ADa%20consultar%20por..." target="_blank" rel="noopener noreferrer">
+            <FaWhatsapp />
           </a>
 
         </div>
